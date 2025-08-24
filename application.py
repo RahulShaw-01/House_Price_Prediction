39aebd7302cfe7e8bfc7ca768a8bd0e7942a521f
Your Name <your@email.com>
2025-08-24 13:01:54 +0530

    Flask port changed

--- application.py
+++ application.py
@@ -47,4 +47,4 @@ def find_free_port():
         return s.getsockname()[1]
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=False)
